4b8d7fef57c21612d11565efcdc93328fbe0c2fe
Ruslan Afanasiev <ruslanafanasiev555@gmail.com>
2023-04-23 19:14:18 +0700

    Черновик вывел на запасной брэнч , т.к. много закоменченного когда который нужно делать для перехода на реакт

--- src/main/java/ru/afanasiev/react2redis/React2redisApplication.java
+++ src/main/java/ru/afanasiev/react2redis/React2redisApplication.java
@@ -1,14 +1,37 @@
 package ru.afanasiev.react2redis;
 
+import io.r2dbc.spi.ConnectionFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer;
+import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator;
+import org.springframework.web.reactive.config.EnableWebFlux;
+
 
 @SpringBootApplication(exclude = {R2dbcAutoConfiguration.class})
 public class React2redisApplication {
 
+
+/*
+	@Bean
+	ConnectionFactoryInitializer initializer(ConnectionFactory connectionFactory) {
+
+		ConnectionFactoryInitializer initializer = new ConnectionFactoryInitializer();
+		initializer.setConnectionFactory(connectionFactory);
+		initializer.setDatabasePopulator(new ResourceDatabasePopulator(new ClassPathResource("app_db.sql")));
+
+		return initializer;
+	}
+
+*/
+
 	public static void main(String[] args) {
 		SpringApplication.run(React2redisApplication.class, args);
 	}
+
+
 
 }
--- src/main/java/ru/afanasiev/react2redis/controllers/PersonController.java
+++ src/main/java/ru/afanasiev/react2redis/controllers/PersonController.java
@@ -8,18 +8,23 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import reactor.core.publisher.Flux;
 import ru.afanasiev.react2redis.models.Person;
+import ru.afanasiev.react2redis.repositories.PeopleRepository;
 import ru.afanasiev.react2redis.security.PersonDetails;
 import ru.afanasiev.react2redis.services.AdminService;
+
 
 
 @Controller
 public class PersonController {
     private final AdminService adminService;
+    private final PeopleRepository peopleRepository;
 
     @Autowired
-    public PersonController(AdminService adminService) {
+    public PersonController(AdminService adminService, PeopleRepository peopleRepository) {
         this.adminService = adminService;
+        this.peopleRepository = peopleRepository;
     }
 
     @GetMapping("/hello")
@@ -37,6 +42,15 @@ public class PersonController {
 
         return "hello";
     }
+ /*   @GetMapping("/showUserInfoReact")
+    public Flux<Person> showUserInfoReact(String s) {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        PersonDetails personDetails = (PersonDetails) authentication.getPrincipal();
+        System.out.println(personDetails.getPerson());
+
+        return peopleRepository.findById(s);
+    }*/
 
 
     @GetMapping("/admin")
